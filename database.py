23f5555796af7d03c9f30bf11162bd5dbb4f5099
yiiu-dttn <dangthanhnhidtn@gmail.com>
2025-07-25 08:53:05 +0700

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# ⚠️ Thay thông tin kết nối nếu cần
 DATABASE_URL = "postgresql://postgres:11111111@localhost:5432/MIS_NHI"
 
 engine = create_engine(DATABASE_URL)
