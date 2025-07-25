04db4d4c2077180ff50636891e5590b8d28634a5
yiiu-dttn <dangthanhnhidtn@gmail.com>
2025-07-25 08:54:52 +0700

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,14 +1,17 @@
 from pydantic import BaseModel, Field
 
 class SurveySummaryResponse(BaseModel):
-    survey_id: int = Field(..., alias="SURVEY_ID")
-    nam_khao_sat: int = Field(..., alias="NAM_KHAO_SAT")
-    ty_le_rat_khong_hai_long: float = Field(..., alias="Tỉ lệ Rất không hài lòng")
-    ty_le_khong_hai_long: float = Field(..., alias="Tỉ lệ Không hài lòng")
-    ty_le_binh_thuong: float = Field(..., alias="Tỉ lệ Bình thường")
-    ty_le_hai_long: float = Field(..., alias="Tỉ lệ Hài lòng")
-    ty_le_rat_hai_long: float = Field(..., alias="Tỉ lệ Rất hài lòng")
-    tong_so_cau_tra_loi_hop_le: int = Field(..., alias="Tổng số câu trả lời hợp lệ")
+    survey_id: int = Field(..., alias="survey_id")
+    nam_khao_sat: int = Field(..., alias="nam_khao_sat")
+    co_so_dao_tao_id: int = Field(..., alias="co_so_dao_tao_id")
+    so_phieu_phat_ra: int = Field(..., alias="so_phieu_phat_ra")
+    so_phieu_thu_vao: int = Field(..., alias="so_phieu_thu_vao")
+    ty_le_rat_khong_hai_long: float = Field(..., alias="ty_le_rat_khong_hai_long")
+    ty_le_khong_hai_long: float = Field(..., alias="ty_le_khong_hai_long")
+    ty_le_binh_thuong: float = Field(..., alias="ty_le_binh_thuong")
+    ty_le_hai_long: float = Field(..., alias="ty_le_hai_long")
+    ty_le_rat_hai_long: float = Field(..., alias="ty_le_rat_hai_long")
+    tong_so_cau_tra_loi_hop_le: int = Field(..., alias="tong_so_cau_tra_loi_hop_le")
 
     model_config = {
         "validate_by_name": True,
